0ac38672413bd2063c1a8dfa17729786025b45a9
Hari Vicky K <harivickyk@outlook.com>
2021-03-29 23:13:40 +0530

    Header Navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
     collapsed = true;
+
+    OnSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
