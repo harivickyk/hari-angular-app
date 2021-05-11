8fc7417df5d17c8817b6bc9fd8950d3eebc59a2d
Hari Vicky K <harivickyk@outlook.com>
2021-05-12 03:54:02 +0530

    Deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyD-V0XgBgY5QtaiWjlqH63qjphfJWy_yAs'
 };
 
 /*
