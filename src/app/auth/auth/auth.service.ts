84cb39c158e4889fff756da9787b651293b3c911
Hari Vicky K <harivickyk@outlook.com>
2021-05-12 03:54:12 +0530

    Deployment

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 import { User } from "./user.model";
 
 export interface AuthResponseData {
@@ -28,7 +29,7 @@ export class AuthService {
     
     signUp(email: string, password: string) {
         return this.http
-        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD-V0XgBgY5QtaiWjlqH63qjphfJWy_yAs',
+        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
@@ -48,7 +49,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         return this.http
-        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD-V0XgBgY5QtaiWjlqH63qjphfJWy_yAs', {
+        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password,
             returnSecureToken: true
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyD-V0XgBgY5QtaiWjlqH63qjphfJWy_yAs'
 };
