a418216b4252d26da13e338b4516558864dbf285
Hari Vicky K <harivickyk@outlook.com>
2021-05-12 01:03:29 +0530

    Modules Segregation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthGuardService } from "./auth/auth.guard";
 import { AuthComponent } from "./auth/auth/auth.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeResolverService } from "./recipes/recipe-start/recipe-resolver.service";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
@@ -16,43 +11,11 @@ const appRoutes: Routes = [
         pathMatch: 'full'
     },
     {
-        path: 'recipes',
-        component: RecipesComponent,
-        canActivate: [AuthGuardService],
-        children: [
-            {
-                path: '',
-                component: RecipeStartComponent
-            },
-            {
-                path: 'new',
-                component: RecipeEditComponent
-            },
-            {
-                path: ':id',
-                component: RecipeDetailComponent,
-                resolve: [RecipeResolverService]
-            },
-            {
-                path: ':id/edit',
-                component: RecipeEditComponent,
-                resolve: [RecipeResolverService]
-            }
-        ]
-    },
-    {
-        path: 'shopping-list',
-        component: ShoppingListComponent
-    },
-    {
-        path: 'auth',
-        component: AuthComponent
-    },
-    {
         path: '**', 
         component: RecipesComponent
     }
 ]
+
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
